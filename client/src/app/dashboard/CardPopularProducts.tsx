f940b6ba4510173a4b8ae0808f09bcfef808746b
ulahvinovich <ulahvinovich@unomaha.edu>
2024-12-05 22:31:20 -0600

    add amages

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -25,7 +25,7 @@ const CardPopularProducts = () => {
               >
                 <div className="flex items-center gap-3">
                   <Image
-                    src={`https://s3-i.s3.us-east-2.amazonaws.com/assets/product${
+                    src={`https://s3-i.s3.us-east-2.amazonaws.com/product${
                       Math.floor(Math.random() * 3) + 1
                     }.png`}
                     alt={product.name}
